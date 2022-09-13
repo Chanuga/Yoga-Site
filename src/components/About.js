e468cf74bbc2bff4154956d5f1a39585b3ca3578
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-09-13 07:31:15 +0300

    added

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,12 @@ import React from 'react';
 // import data
 import { about } from '../data';
 
+// import icons
+import { IoIosArrowDroprightCircle } from 'react-icons/io';
+
 const About = () => {
   // destructure about data
-  const { title, subtitle1, subtitle2, usersIcon } = about;
+  const { title, subtitle1, subtitle2, usersIcon, link } = about;
   return (
     <section className='py-[80px] md:py-[110px] lg:pt-[140px] lg:pb-[180px]'>
       <div className='container mx-auto px-[20px] lg:px-[135px]'>
@@ -27,9 +30,22 @@ const About = () => {
         >
           {subtitle1}
         </p>
-        <p className='md:text-body-md' data-aos='fade-up' data-aos-delay='300'>
+        <p
+          className='md:text-body-md mb-8'
+          data-aos='fade-up'
+          data-aos-delay='300'
+        >
           {subtitle2}
         </p>
+        <div data-aos='fade-up' data-aos-delay='400'>
+          <a
+            className='link flex items-center gap-x-4 hover:gap-x-6 transition-all'
+            href='#'
+          >
+            {link}
+            <IoIosArrowDroprightCircle className='text-2xl' />
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -33,4 +33,5 @@ export const about = {
     'We are distinguished by our unsurpassed motivating atmosphere, knowledgeable staff, and premier exercise equipment, which supports our members in meeting their individual fitness goals.',
   subtitle2:
     'The strength of our heart-felt identity is utilized to inspire every person that steps foot into our gyms to better themselves.',
+  link: 'Join Now',
 };
