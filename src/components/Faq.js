7412a46ad57802333dd5a295ae33c3ad966dce56
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-09-16 08:56:48 +0300

    added

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -14,18 +14,23 @@ const Faq = () => {
       <div className='max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-contain min-h-[1160px]'>
         {/* section title */}
         <div
-          className='section-title-group -space-x-4 max-w-[540px] mx-auto px-4 lg:px-0'
+          className='section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0'
           data-aos='fade-up'
           data-aos-offset='200'
           data-aos-delay='200'
         >
-          <img className='md:hidden' src={icon} alt='' />
+          <img className='lg:hidden' src={icon} alt='' />
           <h2 className='h2 section-title lg:mt-[100px]'>
             {title} <span className='text-primary-200'>.</span>
           </h2>
         </div>
         {/* accordion list */}
-        <div className='flex flex-col gap-y-4 px-4'>
+        <div
+          className='flex flex-col gap-y-4 px-4'
+          data-aos='fade-up'
+          data-aos-offset='300'
+          data-aos-delay='200'
+        >
           {accordions.map((accordion, idx) => {
             return <Accordion accordion={accordion} key={idx} />;
           })}
